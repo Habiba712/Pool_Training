30946537b85e23c102acb8051bf4b8ff29b23b98
HabibaTaliby <habibataliby@gmail.com>
2026-03-05 00:27:26 +0000

    ex08 C00 print_combn

---
+++ C00/ex08/ft_print_combn.c
@@ -0,0 +1,52 @@
+#include <unistd.h>
+
+void	ft_putchar(char c)
+
+{
+	write(1, &c, 1);
+}
+
+
+
+void solve(int n, int index, int start,int *tab){
+
+	int i;
+	if(index == n){
+		i =0;
+		while(i < n){
+			ft_putchar(tab[i++] + '0');
+				}
+			if(tab[0] < (10 - n)){
+				ft_putchar(',');
+				ft_putchar(' ');
+			}
+		return;
+		
+		
+	}
+
+	i= start;
+	while (i <= 9){
+		tab[index] = i;
+		solve(n, index + 1, i + 1, tab);
+	i++;
+		
+	}
+}
+
+void ft_print_combn(int n){
+
+	int tab[10];
+	
+	if (n > 0 && n < 10){
+		solve(n, 0, 0, tab);
+	}
+
+}
+
+
+int main(void){
+	
+	ft_print_combn(2);	
+	return 0;
+}
