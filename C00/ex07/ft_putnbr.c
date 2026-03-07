8ca7a695530f72cb83103876ebe07cf40515d810
HabibaTaliby <habibataliby@gmail.com>
2026-03-07 01:18:34 +0000

    C00 ex07 réevaluation

--- C00/ex07/ft_putnbr.c
+++ C00/ex07/ft_putnbr.c
@@ -1,28 +1,26 @@
 #include <unistd.h>
 
+
 void ft_putchar(char c){
 	write(1, &c, 1);
-
 }
 
 void ft_putnbr(int nb){
-	if ( nb == -2147483647){
+	long n;
+	n = nb;	
+	if (n < 0){
 		ft_putchar('-');
-		ft_putchar(2 + '0');
-		nb = -nb;
+		n = -n;
 	}
-	if (nb < 0){
-		ft_putchar('-');
-		nb = -nb;
+	if(n > 9){
+	ft_putnbr(n / 10);
 	}
-	if(nb > 9){
-	ft_putnbr(nb / 10);
-	}
-	ft_putchar((nb % 10) + '0');
+	ft_putchar((n % 10) + '0');
+	
 
 }
 
 int main(void){
-	ft_putnbr(-2948);
+	ft_putnbr(0);
 	return 0;
 }
--- C00/ex08/ft_print_combn.c
+++ C00/ex08/ft_print_combn.c
@@ -47,6 +47,6 @@ void ft_print_combn(int n){
 
 int main(void){
 	
-	ft_print_combn(2);	
+	ft_print_combn(4);	
 	return 0;
 }
